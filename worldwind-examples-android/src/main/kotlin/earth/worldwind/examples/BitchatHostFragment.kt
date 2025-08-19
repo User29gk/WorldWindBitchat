61688d48482404cf97c89513f6eb8549afe2be9d
User29gk <gauravkumar299gk@gmail.com>
2025-08-20 02:31:49 +0530

    AddedWebview

--- worldwind-examples-android/src/main/kotlin/earth/worldwind/examples/BitchatHostFragment.kt
+++ worldwind-examples-android/src/main/kotlin/earth/worldwind/examples/BitchatHostFragment.kt
@@ -1,59 +1,29 @@
-package earth.worldwind.examples
+package com.example.worldwindkotlin.bitchat
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import com.example.worldwindkotlin.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [androidx.fragment.app.Fragment] subclass.
- * Use the [BitchatHostFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BitchatHostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class BiTChatFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bitchat_host, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_bitchat, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BitchatHostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BitchatHostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val webView: WebView = view.findViewById(R.id.webViewBiTChat)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        // Yahan pe BiTChat ka local URL daalna hoga
+        webView.loadUrl("http://localhost:3000/")  // baad me correct karenge
+
+        return view
     }
 }
